1e55e2c8e304a4f0acbe5f0fff185e995208114e
CarlosP1T <36971479+CarlosP1T@users.noreply.github.com>
2020-06-01 22:58:51 -0500

    Add files via upload

---
+++ Ejercicios_Prolog POOT CARLOS/Ejercicio Práctico 1 (2).pl
@@ -0,0 +1,12 @@
+animal(leon).
+animal(vaca).
+animal(tigre).
+carnivoro(leon).
+carnivoro(tigre).
+
+/*a)*/ 
+es_carnivoro(X):-carnivoro(X).
+
+
+
+
---
+++ Ejercicios_Prolog POOT CARLOS/Ejercicio Práctico 2 (1).pl
@@ -0,0 +1,14 @@
+animal(mammal,tiger,carnivore,stripes).
+animal(mammal,hyena,carnivore,ugly).
+animal(mammal,lion,carnivore,mane).
+animal(mammal,zebra,herbivore,stripes).
+animal(bird,eagle,carnivore,large).
+animal(bird,sparrow,scavenger,small).
+animal(reptile,snake,carnivore,long).
+animal(reptile,lizard,scavenger,small).
+
+/*a)* animal(mammal,X,_,_)*/
+/*b)* animal(mammal,X,carnivore,_)*/  
+/*c)* animal(mammal,X,_,stripes)*/
+/*d) animal(reptiles,X,_,mane)*/
+
